2b1118f30b4eff8275d4dc066d7f91265d8bc72f
Aterocana <dominicimaurizio@gmail.com>
2024-03-10 20:00:22 +0100

    export.sh warning message
    Signed-off-by: Aterocana <dominicimaurizio@gmail.com>

--- export.sh
+++ export.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -p "export files from home folder into the repository?" -n 1 -r
+read -p "export files from repository to into home folder?" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     # backup current configurations
