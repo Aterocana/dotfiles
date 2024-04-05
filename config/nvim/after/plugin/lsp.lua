83ebbe3c39efca2c30a4781bd80e46f69cd20080
Aterocana <dominicimaurizio@gmail.com>
2024-04-05 15:37:16 +0200

    some lsp tweaks
    Signed-off-by: Aterocana <dominicimaurizio@gmail.com>

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -3,17 +3,20 @@ local lsp_zero = require('lsp-zero')
 lsp_zero.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "<M-LeftMouse>", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts) -- see popup definition
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts) -- goto definition
+  vim.keymap.set("n", "<M-LeftMouse>", vim.lsp.buf.definition, opts) -- goto definition with mouse
+  vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts) -- goto type definition
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts) -- goto type definition
+  vim.keymap.set("n", "<leader>vws",vim.lsp.buf.workspace_symbol, opts)
+
+  vim.keymap.set("n", "<space>di", "<cmd>Telescope diagnostics<CR>", opts)
+  vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+  vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+  vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+  vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
+  vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+  vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
 end)
 
 -- to learn how to use mason.nvim with lsp-zero
@@ -47,6 +50,17 @@ require('luasnip.loaders.from_vscode').lazy_load()
 
 local lspkind = require('lspkind')
 cmp.setup({
+	snippet = {
+		expand = function (args)
+			require("luasnip").lsp_expand(args.body)
+		end
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<CR>'] = cmp.mapping.confirm({ select = true }),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<Tab>'] = cmp_action.tab_complete(),
+		['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
+	}),
 	sources = {
 		{name = 'nvim_lsp'},
 		{name = 'buffer', keyword_length = 3},
@@ -70,14 +84,6 @@ cmp.setup({
 			end
 		})
 	},
-	mapping = cmp.mapping.preset.insert({
-		['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
-		['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
-		['<CR>'] = cmp.mapping.confirm({ select = true }),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<Tab>'] = cmp_action.tab_complete(),
-		['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
-	}),
 	experimental = {
 		ghost_text = true
 	},
