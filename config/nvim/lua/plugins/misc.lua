b76b86821bd28bc8dbf8d086a9fb0e1e3111b53b
Aterocana <dominicimaurizio@gmail.com>
2024-11-28 09:10:09 +0100

    nvim style plugins sway keybindings
    Signed-off-by: Aterocana <dominicimaurizio@gmail.com>

--- config/nvim/lua/plugins/misc.lua
+++ config/nvim/lua/plugins/misc.lua
@@ -54,4 +54,42 @@ return {
 			})
 		end
 	},
+	{
+		"atiladefreitas/dooing",
+		config = function()
+			require("dooing").setup({
+				-- Core settings
+				save_path = vim.fn.stdpath('data') .. '/dooing_todos.json',
+
+				-- Window appearance
+				window = {
+					width = 60,         -- Width of the floating window
+					height = 30,        -- Height of the floating window
+					border = 'rounded', -- Border style
+				},
+
+				-- Icons
+				icons = {
+					pending = '○',      -- Pending todo icon
+					done = '✓',        -- Completed todo icon
+				},
+
+				-- Keymaps
+				keymaps = {
+					toggle_window = "<leader>td", -- Toggle the main window
+					new_todo = "i",              -- Add a new todo
+					toggle_todo = "x",           -- Toggle todo status
+					delete_todo = "d",           -- Delete the current todo
+					delete_completed = "D",      -- Delete all completed todos
+					close_window = "q",          -- Close the window
+					toggle_help = "?",           -- Toggle help window
+					toggle_tags = "t",           -- Toggle tags window
+					clear_filter = "c",          -- Clear active tag filter
+				},
+			})
+		end,
+		keys = {
+			{ "<leader>td", desc = "Toggle Todo List" },
+		},
+	},
 }
