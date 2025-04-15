e0a59966e5eb55dad948a0014313257bb4148c48
Aterocana <dominicimaurizio@gmail.com>
2025-04-15 17:35:54 +0200

    powermenu
    Signed-off-by: Aterocana <dominicimaurizio@gmail.com>

--- config/rofi/scripts/clipboard.sh
+++ config/rofi/scripts/clipboard.sh
@@ -9,7 +9,7 @@ rofi_cmd() {
 #		--tool-args='rofi -theme-str "textbox-prompt-colon {str: \"\";}" -theme-str "listview {columns: 1; lines: 7;}" -dmenu -p "Clipboard" -markup-rows -theme ~/.config/rofi/scripts/style.rasi'
 	clipman $1 \
 		--tool=CUSTOM \
-		--tool-args='rofi -theme-str "window {width:900px;}" -theme-str "listview {columns: 1; lines: 7;}" -dmenu -p "" -markup-rows -theme ~/.config/rofi/scripts/clipboard.rasi'
+		--tool-args='rofi -theme-str "window {width:900px;}" -theme-str "listview {columns: 1; lines: 7;}" -dmenu -p " " -markup-rows -theme ~/.config/rofi/scripts/clipboard.rasi'
 }
 
 rofi_cmd $1
