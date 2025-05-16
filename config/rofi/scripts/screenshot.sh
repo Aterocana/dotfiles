5ccf072add72ea9d90656f4840b5cd9fecc143ed
Aterocana <dominicimaurizio@gmail.com>
2025-05-16 14:55:24 +0200

    screenshot.sh is now capable of transcribe text from screen to clipboard
    Signed-off-by: Aterocana <dominicimaurizio@gmail.com>

--- config/rofi/scripts/screenshot.sh
+++ config/rofi/scripts/screenshot.sh
@@ -19,9 +19,10 @@ win_width='400px'
 option_1=" Capture Desktop"
 option_2=" Capture Area"
 option_3=" Capture Window"
-option_4=" Capture in 1s"
-option_5=" Capture in 5s"
-option_6=" Capture in 10s"
+option_4="󱄽 transcribe from screen"
+option_5=" Capture in 1s"
+option_6=" Capture in 5s"
+option_7=" Capture in 10s"
 
 # Rofi CMD
 rofi_cmd() {
@@ -98,6 +99,10 @@ shotarea () {
 	copy_shot
 }
 
+transcribe () {
+	grim -g "$(slurp)" - | tesseract - stdout 2>/dev/null | wl-copy
+}
+
 # Execute Command
 run_cmd() {
 	if [[ "$1" == '--opt1' ]]; then
@@ -107,10 +112,12 @@ run_cmd() {
 	elif [[ "$1" == '--opt3' ]]; then
 		shotwin
 	elif [[ "$1" == '--opt4' ]]; then
-		shot_in_sec '1'
+		transcribe
 	elif [[ "$1" == '--opt5' ]]; then
-		shot_in_sec '5'
+		shot_in_sec '1'
 	elif [[ "$1" == '--opt6' ]]; then
+		shot_in_sec '5'
+	elif [[ "$1" == '--opt7' ]]; then
 		shot_in_sec '10'
 	fi
 }
