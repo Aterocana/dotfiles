c5918f930458210f6cd60bee03dccc742b3704d2
Aterocana <dominicimaurizio@gmail.com>
2025-10-31 15:20:35 +0100

    zsh: fkill use fzf-tmux in popup nvim: picker configurations
    Signed-off-by: Aterocana <dominicimaurizio@gmail.com>

--- config/nvim/after/plugin/pick.lua
+++ config/nvim/after/plugin/pick.lua
@@ -1,6 +1,13 @@
-vim.keymap.set("n", "<leader>pf", require("mini.pick").builtin.files, { desc = "Search for files" })
-vim.keymap.set('n', '<C-g>', require("core.pick").live_grep, { desc = 'Live grep (search file contents)' })
-vim.keymap.set("n", "<leader>di", require("core.pick").pick_diagnostics, { desc = "Search LSP diagnostics" })
-vim.keymap.set("n", "<leader>buf", require("mini.pick").builtin.buffers, { desc = "Search for open buffers" })
-vim.keymap.set("n", "<leader>ph", require("mini.pick").builtin.help, { desc = "Search help tags" })
-vim.keymap.set("n", "<leader>pkm", require("core.pick").pick_keymappings, { desc = "List all key mapping" })
+local pick = require("mini.pick")
+local pickcore = require("core.pick")
+vim.keymap.set("n", "<leader>pf", pickcore.pick_files_filters({
+  '/vendor/',
+  '^vendor/'
+}), { desc = "[P]ick [F]iles" })
+vim.keymap.set('n', '<C-g>', pickcore.live_grep, { desc = 'Live grep (search file contents)' })
+vim.keymap.set("n", "<leader>pd", pickcore.pick_diagnostics, { desc = "[P]ick LSP [D]iagnostics" })
+vim.keymap.set("n", "<leader>pb", pick.builtin.buffers, { desc = "[P]ick open [B]uffers" })
+vim.keymap.set("n", "<leader>ph", pick.builtin.help, { desc = "[P]ick [H]elp pages" })
+vim.keymap.set("n", "<leader>pkm", pickcore.pick_keymappings, { desc = "List all key mapping" })
+vim.keymap.set("n", "<leader>pr", pick.builtin.resume, { desc = "[P]ick [R]esume" })
+vim.keymap.set("n", "<leader>pc", pickcore.custom, { desc = "[P]ick [R]esume" })
