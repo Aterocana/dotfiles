da063393e1eb170f0b4efa2fe7c7077b47e54fcf
Aterocana <dominicimaurizio@gmail.com>
2025-11-04 18:53:56 +0100

    tmux sessions rofi script updated
    Signed-off-by: Aterocana <dominicimaurizio@gmail.com>

--- config/rofi/scripts/tmux_sessions.sh
+++ config/rofi/scripts/tmux_sessions.sh
@@ -5,6 +5,7 @@
 
 TERM=kitty
 NEW="Create a NEW session"
+NOTIFY=notify-send
 
 rofi_cmd() {
 	rofi \
@@ -14,22 +15,38 @@ rofi_cmd() {
 		-p " " \
 		-markup-rows \
 		-theme "$HOME/.config/rofi/styles/style.rasi"
-}
+	}
 
 tmux_sessions() {
-    tmux list-session -F '#S'
+	tmux list-session -F '#S'
 }
+
+# Prompt to ask for a new session name
+rofi_prompt() {
+	rofi \
+		-dmenu \
+		-p " name:" \
+		-theme-str 'entry { placeholder: "session-name"; }' \
+		-theme "$HOME/.config/rofi/styles/style.rasi"
+	}
 
 run_rofi() {
 	local tsessions=$(tmux_sessions)
 	local session=$( echo -e "$NEW\n$tsessions" | rofi_cmd)
 
 	if [[ x"$NEW" = x"${session}" ]]; then
-		$TERM -e tmux new-session &
+		local name=$( rofi_prompt )
+		if [[ -z "$name" ]]; then
+			$NOTIFY "tmux" "No session name provided"
+			exit 0
+		fi
+		$TERM -e tmux new-session -s "$name" &
+		$NOTIFY "tmux" "created session $name"
 	elif [[ -z "${session}" ]]; then
 		echo "Cancel"
 	else
 		$TERM -e tmux attach -t "${session}" &
+		$NOTIFY "tmux" "attached to session $session"
 	fi
 }
 
