7d7c0539478d36a0509ab05eae44ea745b60df8d
Aterocana <dominicimaurizio@gmail.com>
2025-11-06 17:47:09 +0100

    tmux_sessions.sh improved dlogs alias
    Signed-off-by: Aterocana <dominicimaurizio@gmail.com>

--- home/.zsh/k8s.zsh
+++ home/.zsh/k8s.zsh
@@ -3,6 +3,7 @@ alias mk="minikube"
 alias kns="kubens"
 alias kctx="kubectx"
 alias klogs="kubectl get pods | tail -n +2 | fzf-tmux -p | awk {'print \$1'} | xargs -I{} kubectl logs -f {}"
+alias dlogs='docker ps --format "table {{.Names}}\t{{.Image}}\t{{.Status}}\t{{.Ports}}" | tail -n +2 | fzf-tmux -p | awk {"print \$1"} | xargs -I{} docker logs --tail 100 -f {}'
 
 # kdesc <entity_type> gets all entity with provided type in current namespace, present them in a fzf popup and describe the resource you selected.
 function kdesc () {
